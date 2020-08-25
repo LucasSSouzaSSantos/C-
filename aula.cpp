e831536913971d22b490ebad77a592fdbacaa48f
Lucas <lucsousan@gmail.com>
2020-08-24 21:23:38 -0300

    fim da aula 01

--- aula.cpp
+++ aula.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 int main(int argc, char const *argv[]){
 
-    cout <<"Ola mundo";
-    
+    cout << "Ola mundo";
+    cout << "Oi já foi";
+
     return 0;
+    /*
+    Processador   
+    HD 
+                   as coisas que baixamos fica armazenado no hd do computador
+    
+    Memória RAM    as coisas que rodam no computador é copiado do hd e é jogado para 
+                   memória ram
+    */
 }
