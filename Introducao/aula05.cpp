6ca5a3cf99567bd29bfd854c7c55cde760116277
Lucas <lucsousan@gmail.com>
2020-08-26 21:56:37 -0300

    fim da aula 05

---
+++ Introducao/aula05.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+// diretiva define para usar uma constante
+
+#define pi 3.141592     // pi é uma constante e ela estar declarada globalmente
+
+int main(){
+
+    // declaração de multiplas variáveis
+
+    int vidas, tiros, life;
+
+    cout << pi << "\n\n";
+
+    return 0;
+}
