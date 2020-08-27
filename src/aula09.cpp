7b9228ae67fac8dad4630e25b39f7726e6597d91
Lucas <lucsousan@gmail.com>
2020-08-27 08:59:27 -0300

    fim da aula 09

---
+++ src/aula09.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int num1 = 10, num2 = 50;
+    char opc = 's';
+
+    if(num1 > num2 && opc == 's'){
+        cout << "Valor de num1 MAIOR ou IGUAL que num2" << "\n";
+    }else{
+        cout << "Valor de num2 MAIOR ou IGUAL que num2" << "\n";
+    }
+
+
+    return 0;
+}
