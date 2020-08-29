c57872ae81798d4a55fdbb737805847512903b7f
Lucas <lucsousan@gmail.com>
2020-08-28 23:01:47 -0300

    modificado

--- src/aula11.cpp
+++ src/aula11.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <stdlib.h>
+// #include <locale.h>
 #include <time.h>
 
 using namespace std;
 
 int main(){
     // operadores lógicos and(&&), or(||) e not(!)
-    system("mv aula /home/lucas/Documentos/GitHub/Cpp/src/introducao_cpp");
+    int num;
+    /*
+        1 2 3 4 5 6 7 8 9 10 
+        >4 e <7
+    */
+   system("clear");
+   num = 8;
+   if(num > 4 && num < 7){
+       cout << "\n\nValor aceito\n";
+   }else{
+       cout << "\n\nNão aceito\n";
+   }
+
     return 0;
 }
