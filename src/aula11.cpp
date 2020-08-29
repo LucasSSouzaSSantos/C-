e75dbff466d9e7a8c00f94f66533aa56601ac252
Lucas <lucsousan@gmail.com>
2020-08-28 22:28:15 -0300

    aula 11 fazendo testes

---
+++ src/aula11.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main(){
+    cout << "Voce estar no diretório\n";
+    system("clear");
+    system("pwd");
+    system("clear");
+
+    return 0;
+}
