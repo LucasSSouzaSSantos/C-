c26da65df667c3594027a6a89a2a9cbbe98fba4d
Lucas <lucsousan@gmail.com>
2020-09-10 22:10:37 -0300

    FINAL DA AULA 17

--- src/basic/aula17.cpp
+++ src/basic/aula17.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
 
 using namespace std;
 
 int main(){
 
+    system("clear");
+    int x;
+    for(x = 0; x < 10; x++){
+        cout << x << " ";
+    }
+    cout << "\n";
+    // system("PAUSE");
     return 0;
 }
