35ad483546e8982ce7bf350a37cd09fc50450967
Lucas <lucsousan@gmail.com>
2020-09-10 22:24:16 -0300

    final da aula 18

--- src/arrays/aula18.cpp
+++ src/arrays/aula18.cpp
@@ -13,5 +13,27 @@ int main(){
 
     // posições começando de 0 até a posição 9
 
+    // essa é a maneira de colocar valores em cada posição do vetor de 10 posições
+
+    vetor[0] = 10;  
+    vetor[1] = 0;
+    vetor[2] = 1;
+    vetor[3] = 2;
+    vetor[4] = 3;
+    vetor[5] = 4;
+    vetor[6] = 5;
+    vetor[7] = 6;
+    vetor[8] = 7;
+    vetor[9] = 8;
+
+    // imprimindo os valores do vetor
+    cout << vetor[0] << "\n";
+
+    // ou pode ser usado um laço de repetição
+
+    for(int i = 0; i < 10; i++){
+        cout << vetor[i] << " ";
+    }
+    cout << "\n";
     return 0;
 }
