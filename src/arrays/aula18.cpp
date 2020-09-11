85b49554370e8bc91e3a2288730f991693956527
Lucas <lucsousan@gmail.com>
2020-09-10 22:16:15 -0300

    aula 18

---
+++ src/arrays/aula18.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+    system("clear");
+    printf("hello world!\n");
+
+    // como declarar um vetor
+
+    int vetor[10]; // declaração de um vetor de 10 posições
+
+    // posições começando de 0 até a posição 9
+
+    return 0;
+}
