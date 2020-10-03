d09253c044b7cc630d6aef0115db900f85f25ffe
Lucas <lucsousan@gmail.com>
2020-10-02 23:37:12 -0300

    Aula 25

---
+++ src/funcoes/Aula24.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+// Prototipar as funções
+
+void impressao(string txt = "vazio");
+
+int main(){
+    // Omissão de Argumentos
+    system("clear");
+
+    impressao();
+
+
+    return 0;
+}
+
+void impressao(string txt){
+    cout << "\n" << txt << "\n";
+}
