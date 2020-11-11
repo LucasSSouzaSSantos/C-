bfb842f73ff68355c4542f1bbe6ea05fc50c5120
Lucas <lucsousan@gmail.com>
2020-11-11 15:41:27 -0300

    mudanças

---
+++ estrutura_de_dados/aula2/aula02.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Ponto.h" // inclui os Protótipos
+
+
+int main(){
+    /***
+     * A convenção em linguagem C é preparar dois arquivos para
+     * implementar um "TAD";
+     * 
+     * Arquivo ".H": protótipos das funções, tipos de ponteiro, 
+     * e dados globalmente acessível;
+     * 
+     * Arquivo ".C": declaração do tipo de dados e implementação
+     * das suas funções.
+     * 
+     * Assim separamos o "conceito" (definição do tipo) de sua 
+     * implementação.
+     * 
+     * Exemplo de "TAD": um ponto definido por suas coordenadas 
+     * "x" e "y".
+     * 
+     * struct ponto{
+     *      float x;
+     *      float y;
+     * };
+     * 
+     * 1º passo: definir o arquivo ".H"
+     *      - protótipos das funções
+     *      - tipos de ponteiro
+     *      - dados globalmente acessível
+     * 
+     * **/
+    return 0;
+}
---
+++ estrutura_de_dados/aula_1/aula1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    /***
+     * Tipo de dado: define o conjunto de valores
+     * (domínio) que uma variável pode assumir.
+     * 
+     * Ex: int
+     * 
+     * ... -2, -1, 0, +1, +2,...
+     * 
+     * Estruturas de Dados: trata-se de um 
+     * relacionamento lógico entre tipos de dados 
+     * 
+     * Tipos Abstratatos de Dados ou "TAD": incluem
+     * as operações para a manipulação dos dados
+     * 
+     * Ex:
+     *    - criação da estrutura
+     *    - inclusão de um elemento
+     *    - remoção de um elemento
+     *    - acesso a um elemento
+     *    - etc
+     * 
+     *  Vantagens: 
+     *  - Encapsulamento e Segurança: usuário não tem
+     * acesso direto aos dados.
+     * 
+     *  - Flexibilidade e Reutulização: podemos alterer
+     * o "TAD" sem alterar as aplicações que o utilizam.
+     * 
+     * Exemplo de TAD: arquivos em C
+     * 
+     * File* f;
+     * 
+     * Os dados de "f" somente podem ser acessados
+     * pelas funções de manipulação do tipo: 
+     *  - fopen()
+     *  - fclose()
+     *  - fputc()
+     *  - fgetc()
+     *  - feof()
+     *  - ...  
+     * **/
+    
+    return 0;
+}
